cede263ef089bcb0a6b58af08360e5de8142f5fe
Thomas GUIRRIEC <thomas@guirriec.fr>
2022-03-07 20:24:33 +0000

    Update linky_exporter.py

--- linky_exporter.py
+++ linky_exporter.py
@@ -177,6 +177,7 @@ class LinkyCollector():
         while b'\x02' not in line:
             if time.time() > frame_timeout_start + frame_timeout:
                 logging.error("No Linky Frame Received !")
+                self.ser.close()
                 sys.exit(1)
             logging.debug("Wait For New Linky Frame")
             line = self.ser.readline()
